548c64b88dc1eed22c3bca48a70c0f01f71c0a05
royvardhan <royvardhan.official@gmail.com>
2022-08-09 07:07:48 +0530

    .

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -6,6 +6,7 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle_NotEnoughETHEntered();
+error Rafale_TransferFailed();
 
 contract Raffle is VRFConsumerBaseV2 {
 
@@ -17,8 +18,11 @@ contract Raffle is VRFConsumerBaseV2 {
     uint32 private immutable i_callbackGasLimit;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
+    address public s_recentWinner;
 
-    event LogPlayerEntered(address indexed player, uint256 amount);
+    event RaffleEnter(address indexed player, uint256 amount);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event RecentWinner(address indexed winner);
 
     constructor(uint256 entranceFee, address vrfCoordinatorv2, bytes32 keyHash, uint64 subscriptionId, uint32 callbackGasLimit ) VRFConsumerBaseV2(vrfCoordinatorv2) {
         i_entranceFee = entranceFee;
@@ -38,28 +42,40 @@ contract Raffle is VRFConsumerBaseV2 {
         revert Raffle_NotEnoughETHEntered();
     }
     s_players.push(payable(msg.sender));
-    emit LogPlayerEntered(msg.sender, msg.value);
+    emit RaffleEnter(msg.sender, msg.value);
     
    }
 
    function pickRandomWinner() external {
-    i_vrfCoordinator.requestRandomWords(
+    uint256 requestId = i_vrfCoordinator.requestRandomWords(
       i_keyHash,
       i_subscriptionId,
       REQUEST_CONFIRMATIONS,
       i_callbackGasLimit,
       NUM_WORDS
     );
+    emit RequestedRaffleWinner(requestId);
    
    }
 
    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
-
+    uint256 indexOfWinner = randomWords[0] % s_players.length;
+    address payable recentWinner = s_players[indexOfWinner];
+    s_recentWinner = recentWinner;
+    (bool success, ) = recentWinner.call{value: address(this).balance}("");
+    if (!success) {
+        revert Rafale_TransferFailed();
+    }
+    emit RecentWinner(recentWinner);
    }
 
    function getPlayer(uint256 playerIndex) public view returns (address) {
        return s_players[playerIndex];
    }
-    
+
+   function getRecentWinner() public view returns (address) {
+       return s_recentWinner;
+   }
+
    } 
 
